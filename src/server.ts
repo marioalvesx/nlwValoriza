252b701e009e67a8729768325edbfd8fa838beaf
Mario Alves <mario.neto@umbler.com>
2021-06-23 02:35:37 -0300

    Aulas 2 & 3

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,29 @@
-import express, { request, response } from "express";
+import "reflect-metadata";
+import express, { Request, Response, NextFunction } from "express";
+import "express-async-errors";
+
+import { router } from "./routes";
+
+import "./database";
 
 const app = express();
 
-app.get("/test", (request, response) => {
-  return response.send("Ola");
-})
+app.use(express.json()); 
 
-app.post("/test-post", (request, response) => {
-  return response.send("Ola NLW - Método post")
+app.use(router);
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+  if(err instanceof Error) {
+    return response.status(400).json({
+      error: err.message
+    })
+  }
+
+  return response.status(500).json({
+    status: "error",
+    message: "Internal Server Error"
+  })
 })
 
 app.listen(3000, () => console.log("Server is running!"))
